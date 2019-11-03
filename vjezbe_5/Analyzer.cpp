640389f59944c6eaf0051fe4b27abc40e0a4367b
jgabr-55 <jgabricevic@gmail.com>
2019-11-03 17:07:44 +0100

    vj5_gotovo

--- vjezbe_5/Analyzer.cpp
+++ vjezbe_5/Analyzer.cpp
@@ -3,15 +3,24 @@
 #include <TH2.h>
 #include <TStyle.h>
 #include <TCanvas.h>
+#include <TLegend.h>
+#include <THStack.h>
+#include <TLorentzVector.h>
 
 void Analyzer::Loop()
 
 
 {
+	//void SetHistLineColor(Color_t color = 1);
 
-	TCanvas *c = new TCanvas("c","c",900,900);
+	TCanvas *c = new TCanvas("c","c",1800,900); // brojevi se odnose na dimenzije slike
+	c->Divide(2,0); //broj stupaca, broj redaka
+	
 
-	TH1F *h1 = new TH1F("trans_kol_gib","naslov12",67,0,78);
+	TH1F *h1 = new TH1F("trans_kol_gib_1","",100,0,100); //brojevi se odnose na binove i sl.
+	TH1F *h2 = new TH1F("trans_kol_gib_2","",100,0,100);
+	TH1F *h3 = new TH1F("","",100,0,100);
+	TLorentzVector *Higgs, *part_1, *part_2;
 
 //   In a ROOT session, you can do:
 //      root> .L Analyzer.C
@@ -48,11 +57,71 @@ void Analyzer::Loop()
       // if (Cut(ientry) < 0) continue;
 	//cout<<EventNumber<<endl;
 
-	h1->Fill(Particle1pt);
+	h1->Fill(Particle1pt); //histogram ("ono cime ga punimo na x os")
+	h2->Fill(Particle2pt);
+	
+	//koristeći vektore dobijemo komponente kolicine gibanja za Higgsa
+	part_1 = new TLorentzVector(Particle1px,Particle1py,Particle1pz,Particle1E);
+	part_2 = new TLorentzVector(Particle2px,Particle2py,Particle2pz,Particle2E);
+	Higgs=new TLorentzVector();
+
+	*Higgs = *part_1 + *part_2;
+
+
+	double tmp = sqrt((Higgs->Px())*(Higgs->Px())+(Higgs->Py())*(Higgs->Py())); //pt za Higgs
+	h3->Fill(tmp);
    }
 
+//h1->SetHistLineColor(Color_t color = 1);
+c->cd(1);//postavljanje aktivnog prozora
+h1->GetXaxis()->SetTitle("Transverzalna kolicina gibanja");
+h1->GetYaxis()->SetTitle("Broj pojavljivanja");
+h1->SetLineColor(2);
+h1->SetFillColor(2);
+h1->SetTitle("Histogram transverzalne kolicine gibanja cestica 1 i 2");
+
+h2->SetLineColor(209);
+//h2->SetFillColor(1);
+
+h3->SetLineColor(4);
 h1->Draw();
+h2->Draw("SAME");
+c->cd(2);
+
+h3->SetTitle("Histogram transverzalne kolicine gibanja rekonstruiranog Higgs bozona");
+h3->GetXaxis()->SetTitle("Transverzalna kolicina gibanja");
+h3->GetYaxis()->SetTitle("Broj pojavljivanja");
+h3->Draw("SAME");
+
+gStyle->SetOptStat(0);
+
+
+
+//h1->Draw(); //histogram
+//h2->Draw();
+/*THStack *hs = new THStack("hs","Transverzalna kol gibanja");
+hs->Add(h1);
+hs->Add(h2);*/
+
+//Crtamo legendu za cestice s pt1 i pt2
+c->cd(1);
+TLegend *legend = new TLegend(0.1,0.8,0.3,0.9);
+//legend->SetHeader("Header","C");
+
+legend->AddEntry(h1,"Cestica1","f");
+legend->AddEntry(h2,"Cestica2","l");
+legend->Draw();
+
+//crtamo legendu za rekonstruirani Higgs bozon
+c->cd(2);
+TLegend *legend1 = new TLegend(0.1,0.8,0.3,0.9);
+legend1->AddEntry(h3,"Rekonstruirani Higgs bozon","l");
+legend1->Draw();
+//legend->SetTextAlign(13);
+
 c->SaveAs("histo.pdf");
+c->SaveAs("histo.png");
+c->SaveAs("histo.root");
 
 }
 
